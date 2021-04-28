bd66c2e0006ea84e7c2e81c8c84251fb329014f0
David-Diniz <79600133+David-Diniz@users.noreply.github.com>
2021-04-27 21:34:59 -0400

    Update Clear screen function.py

--- Clear screen function.py
+++ Clear screen function.py
@@ -1,8 +1,9 @@
-import os
-cls = lambda: os.system("cls")
+#for the longest time i could never clear the console in python, this is how i did it
+import os #os is needed
+cls = lambda: os.system("cls") #makes a lambda function called cls which will clear the screen
 for i in range (5):
-    print ("HAHAHAHAHAHA")
+    print ("HAHAHAHAHAHA")#will print this 5 times,cluttering the screen
 
-input()
-cls()
+input()#makes you press enter to clear the screen
+cls()#uses the clear function created earlier
 print("Stuff was once here")
