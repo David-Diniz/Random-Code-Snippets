a92d8152c89c34c2ff7d82f0da8d733a10f1d8ce
David-Diniz <79600133+David-Diniz@users.noreply.github.com>
2021-05-01 12:42:55 -0400

    Update TXT_files.py

--- TXT_files.py
+++ TXT_files.py
@@ -1,3 +1,4 @@
+#the purpose of this file is to allow the user to input anything they like until they type, each input is then outputed to a txt file which the user can then open and read
 file = open("output.txt", "w") #opens a .txt file called output and if one isn't found it creates a new one
 while True: #while true simply puts it into an infinite loop
     current = input()#this is where the user inputs text
