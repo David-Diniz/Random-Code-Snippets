c1adca2b2c4e9caff3cc25e769d3a60f64bf1243
David-Diniz <79600133+David-Diniz@users.noreply.github.com>
2021-05-01 12:41:52 -0400

    Add files via upload

---
+++ TXT_files.py
@@ -0,0 +1,8 @@
+file = open("output.txt", "w") #opens a .txt file called output and if one isn't found it creates a new one
+while True: #while true simply puts it into an infinite loop
+    current = input()#this is where the user inputs text
+    if (current.lower() == "done"): #checks if the user has input done
+        break #since the program is supposed to stop when it detects done, if it does this will break from the loop
+    file.write(current + "\n")#this line appends the user's text to the txt file and starts a new line for the next input
+
+file.close();#closes output.txt
